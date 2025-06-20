55e43c318c686485efe9dde3e82f8bcb1073ace6
drakkar78 <122934629+drakkar78@users.noreply.github.com>
2025-06-20 10:02:57 -0500

    Mejoras de seguridad y configuracion

--- functions/dbconn.php
+++ functions/dbconn.php
@@ -1,29 +1,33 @@
 <?php
-//define('ENV_FILE', '/u01/vhosts/inout.upeu.edu.pe/httpdocs/koha-inout/fronts/koha-inout-lima/.env');
-//require_once ENV_FILE;
+$envPath = dirname(__DIR__) . '/.env';
+$env = [];
+if (file_exists($envPath)) {
+    $env = parse_ini_file($envPath, false, INI_SCANNER_TYPED);
+}
 
-//	$servername  = getenv('INOUT_DB_HOST');
-	$servername = "consola-mariadb";
-	$username = "Uinoutl";
-	$password = "DbL1n0u72023#$";
-	$db = "inout_bul";
-	$conn = mysqli_connect($servername, $username, $password, $db);
-	if (!$conn) {
-	    die("Connection failed: " . mysqli_connect_error($conn));
-	}
+$servername = $env['INOUT_DB_HOST'] ?? 'consola-mariadb';
+$username   = $env['INOUT_DB_USER'] ?? 'Uinoutl';
+$password   = $env['INOUT_DB_PASS'] ?? 'DbL1n0u72023#$';
+$db         = $env['INOUT_DB_NAME'] ?? 'inout_bul';
 
-    	$kohaServername = "consola-mariadb";
-	$kohaUsername = "koha_bul";
-	$kohaPassword = 'rP"K)|k#TjQEHs8w';
-    	$kohaDb = "koha_bul";
-    	$koha = mysqli_connect($kohaServername, $kohaUsername, $kohaPassword, $kohaDb);
-    	if (!$koha) {
-        	die("Connection failed: " . mysqli_connect_error($koha));
-    	}
+$conn = mysqli_connect($servername, $username, $password, $db);
+if (!$conn) {
+    die('Connection failed: ' . mysqli_connect_error($conn));
+}
 
-	function sanitize($conn, $str){
-		return mysqli_real_escape_string($conn, $str);
-	}
-	// var_dump(function_exists('mysqli_connect'));
-	date_default_timezone_set("America/Lima");
-?>
+$kohaServername = $env['KOHA_DB_HOST'] ?? 'consola-mariadb';
+$kohaUsername   = $env['KOHA_DB_USER'] ?? 'koha_bul';
+$kohaPassword   = $env['KOHA_DB_PASS'] ?? 'rP"K)|k#TjQEHs8w';
+$kohaDb         = $env['KOHA_DB_NAME'] ?? 'koha_bul';
+
+$koha = mysqli_connect($kohaServername, $kohaUsername, $kohaPassword, $kohaDb);
+if (!$koha) {
+    die('Connection failed: ' . mysqli_connect_error($koha));
+}
+
+function sanitize($conn, $str)
+{
+    return mysqli_real_escape_string($conn, $str);
+}
+
+date_default_timezone_set('America/Lima');
--- functions/signout.php
+++ functions/signout.php
@@ -1,5 +1,5 @@
 <?php
-	session_start();
-	session_destroy();
-	header("Location:../login.php?msg=2");
+session_start();
+session_destroy();
+header('Location:../login.php?msg=2');
 ?>

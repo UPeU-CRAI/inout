54706d99a64d2e0cf399ac91dcd7964178e3d20e
drakkar78 <122934629+drakkar78@users.noreply.github.com>
2025-06-23 05:46:51 -0500

    Restrict access to phpinfo

--- info.php
+++ info.php
@@ -1,1 +1,10 @@
-<?php phpinfo();
+<?php
+session_start();
+
+// Redirect to login page if the user is not authenticated
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit();
+}
+
+phpinfo();

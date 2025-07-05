14a81d5da3171c9dd5ec98b6ba2f65dd545d38ce
drakkar78 <122934629+drakkar78@users.noreply.github.com>
2025-07-05 05:01:10 -0500

    Return TTS audio as JSON

--- tts.php
+++ tts.php
@@ -1,11 +1,22 @@
 <?php
 require_once __DIR__ . '/functions/PersonalizedGreeting.php';
 
-header('Content-Type: text/html; charset=utf-8');
+header('Content-Type: application/json; charset=utf-8');
 
 $text = trim($_POST['text'] ?? '');
+$tts = new PersonalizedGreeting();
+
+$male  = '';
+$female = '';
+
 if ($text !== '') {
-    $tts = new PersonalizedGreeting();
-    echo $tts->synthesizeVoice($text);
+    $male   = $tts->synthesizeVoiceData($text, 'M') ?? '';
+    $female = $tts->synthesizeVoiceData($text, 'F') ?? '';
 }
+
+echo json_encode([
+    'audio_male'   => $male,
+    'audio_female' => $female,
+]);
+
 

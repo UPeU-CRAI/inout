cb61c94711ba406e03dfe625492967feab7326e9
drakkar78 <122934629+drakkar78@users.noreply.github.com>
2025-07-05 22:26:03 -0500

    Add Azure Speech SDK and autoload

--- functions/AzureSpeech.php
+++ functions/AzureSpeech.php
@@ -1,10 +1,10 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/env_loader.php';
+
 use Microsoft\CognitiveServices\Speech\SpeechConfig;
 use Microsoft\CognitiveServices\Speech\SpeechSynthesizer;
 use Microsoft\CognitiveServices\Speech\ResultReason;
-
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/env_loader.php';
 
 class AzureSpeech
 {
